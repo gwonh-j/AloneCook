0edad3e5e10b83761cb058873ed36625137bc3a5
Gwonhee Jo <gwonheej@gmail.com>
2024-03-04 20:02:20 +0900

    question and answer func

--- djangocrm/Alone_Cook/urls.py
+++ djangocrm/Alone_Cook/urls.py
@@ -9,6 +9,8 @@ urlpatterns = [
     # 게시글 제목의 a 태그에 href 속성에 해당하는 동적 URL 매핑
     path("<int:question_id>", views.detail, name= 'detail'),
 
-    
-    path("answer/create/<int:question_id>/", views.answer_create, name="answer_create")
+    path("answer/create/<int:question_id>/", views.answer_create, name="answer_create"),
+
+    path('question/create/', views.question_create, name='question_create'),
 ]
+
--- djangocrm/Alone_Cook/views.py
+++ djangocrm/Alone_Cook/views.py
@@ -1,7 +1,8 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.http import HttpResponse
+from django.http import HttpResponse, HttpResponseNotAllowed
 from .models import Question, Answer
 from django.utils import timezone
+from .forms import QuestionForm, AnswerForm
 
 def index(request):
     question_list = Question.objects.order_by('-create_date') # -가 들어가면 내림차순
@@ -18,6 +19,29 @@ def detail(request, question_id):
 def answer_create(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
 
-    # 아래 create 함수는 Answer 클래스 모델을 가져와 저장할 때 사용하는 save()랑 동일한 기능
-    a = question.answer_set.create(content=request.POST.get("content"), create_date = timezone.now())
-    return redirect("Alone_Cook:detail", question_id= question.id)
+    if request.method == "POST":
+        form = AnswerForm(request.POST)
+        if form.is_valid():
+            answer = form.save(commit=False)
+            answer.create_date = timezone.now()
+            answer.question = question
+            answer.save()
+            return redirect('Alone_Cook:detail', question_id=question.id)
+    else:
+        return HttpResponseNotAllowed('Only POST is possible.')
+    context = {'question': question, 'form': form}
+    return render(request, 'Alone_Cook/question_detail.html', context)
+
+
+def question_create(request):
+    if request.method == 'POST': # post 요청일 때는 입력한 값들을 전달받아 DB에 저장
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('Alone_Cook:index')
+    else: # Get 요청일 때는 단순히 질문등록 페이지 요청
+        form = QuestionForm()
+    context = {'form': form}
+    return render(request, 'Alone_Cook/question_form.html', context)
