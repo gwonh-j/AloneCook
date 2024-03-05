308d76bf8d06ebe5256f9686c351b7773849fe96
Gwonhee Jo <gwonheej@gmail.com>
2024-03-05 22:04:35 +0900

    signup

--- djangocrm/common/urls.py
+++ djangocrm/common/urls.py
@@ -8,4 +8,5 @@ urlpatterns = [
     # Django에서 지원하는 views 라이브러리를 활용 (로그인, 로그아웃, 비밀번호 찾기 & 초기화 등)
     path('login/', auth_views.LoginView.as_view(template_name = "common/login.html"), name = "login"),
     path('logout/', views.logout_view, name='logout'), 
+    path('signup/', views.signup, name='signup'),
 ]   
