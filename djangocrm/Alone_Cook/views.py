b76c5c7408d0389703614c265878be2072e53ce1
Gwonhee Jo <gwonheej@gmail.com>
2024-03-06 23:56:15 +0900

    writer func

--- djangocrm/Alone_Cook/views.py
+++ djangocrm/Alone_Cook/views.py
@@ -1,9 +1,9 @@
 from django.shortcuts import render, get_object_or_404, redirect
-from django.http import HttpResponse, HttpResponseNotAllowed
 from .models import Question, Answer
 from django.utils import timezone
 from .forms import QuestionForm, AnswerForm
 from django.core.paginator import Paginator
+from django.contrib.auth.decorators import login_required
 
 def index(request):
     page = request.GET.get('page', '1')  # 페이지
@@ -23,6 +23,7 @@ def detail(request, question_id):
     context = {'question' : question}
     return render(request, 'Alone_Cook\question_detail.html', context)
 
+@login_required(login_url='common:login')
 def answer_create(request, question_id):
     question = get_object_or_404(Question, pk=question_id)
 
@@ -30,21 +31,23 @@ def answer_create(request, question_id):
         form = AnswerForm(request.POST)
         if form.is_valid():
             answer = form.save(commit=False)
+            answer.author = request.user  # author 속성에 로그인 계정 저장
             answer.create_date = timezone.now()
             answer.question = question
             answer.save()
             return redirect('Alone_Cook:detail', question_id=question.id)
     else:
-        return HttpResponseNotAllowed('Only POST is possible.')
+        form = AnswerForm()
     context = {'question': question, 'form': form}
     return render(request, 'Alone_Cook/question_detail.html', context)
 
-
+@login_required(login_url='common:login')
 def question_create(request):
     if request.method == 'POST': # post 요청일 때는 입력한 값들을 전달받아 DB에 저장
         form = QuestionForm(request.POST)
         if form.is_valid():
             question = form.save(commit=False)
+            question.author = request.user  # author 속성에 로그인 계정 저장
             question.create_date = timezone.now()
             question.save()
             return redirect('Alone_Cook:index')
