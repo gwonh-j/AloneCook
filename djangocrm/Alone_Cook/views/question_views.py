3aceb972f92e1a49910eb54f6fd9f0348caa3c07
Gwonhee Jo <gwonheej@gmail.com>
2024-03-08 22:23:28 +0900

    view.py separation

---
+++ djangocrm/Alone_Cook/views/question_views.py
@@ -0,0 +1,49 @@
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+
+from ..forms import QuestionForm
+from ..models import Question
+
+@login_required(login_url='common:login')
+def question_create(request):
+    if request.method == 'POST': # post 요청일 때는 입력한 값들을 전달받아 DB에 저장
+        form = QuestionForm(request.POST)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.author = request.user  # author 속성에 로그인 계정 저장
+            question.create_date = timezone.now()
+            question.save()
+            return redirect('Alone_Cook:index')
+    else: # Get 요청일 때는 단순히 질문등록 페이지 요청
+        form = QuestionForm()
+    context = {'form': form}
+    return render(request, 'Alone_Cook/question_form.html', context)
+
+@login_required(login_url='common:login')
+def question_modify(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    if request.user != question.author:
+        messages.error(request, '수정권한이 없습니다')
+        return redirect('Alone_Cook:detail', question_id=question.id)
+    if request.method == "POST":
+        form = QuestionForm(request.POST, instance=question)
+        if form.is_valid():
+            question = form.save(commit=False)
+            question.modify_date = timezone.now()  # 수정일시 저장
+            question.save()
+            return redirect('Alone_Cook:detail', question_id=question.id)
+    else:
+        form = QuestionForm(instance=question)
+    context = {'form': form}
+    return render(request, 'Alone_Cook/question_form.html', context)
+
+@login_required(login_url='common:login')
+def question_delete(request, question_id):
+    question = get_object_or_404(Question, pk=question_id)
+    if request.user != question.author:
+        messages.error(request, '삭제권한이 없습니다')
+        return redirect('Alone_Cook:detail', question_id=question.id)
+    question.delete()
+    return redirect('Alone_Cook:index')
--- djangocrm/djangocrm/urls.py
+++ djangocrm/djangocrm/urls.py
@@ -1,11 +1,11 @@
 from django.contrib import admin
 from django.urls import include, path
-from Alone_Cook import views
+from Alone_Cook.views import base_views
 
 urlpatterns = [
     path('common/', include('common.urls')),
     path('Alone_Cook/', include('Alone_Cook.urls')),
     path('admin/', admin.site.urls),
-    path('', views.index, name='index'),  # '/' 에 해당되는 path
+    path('', base_views.index, name='index'),  # '/' 에 해당되는 path
     
 ]
