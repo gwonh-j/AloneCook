a65caf2f84c4090699cd663dddc4ef5be2d240ea
Gwonhee Jo <gwonheej@gmail.com>
2024-03-17 22:34:48 +0900

    comment func ver1

--- djangocrm/Alone_Cook/models.py
+++ djangocrm/Alone_Cook/models.py
@@ -22,9 +22,9 @@ class Answer(models.Model):
     voter = models.ManyToManyField(User, related_name='voter_answer')
 
 class Comment(models.Model):
-    author = models.ForeignKey(User, on_delete=models.CASCADE, related_name='author_comment')
-    question = models.ForeignKey(Question, on_delete= models.CASCADE) # Question을 외래키로, 삭제시 참조되는 모든걸 삭제
+    author = models.ForeignKey(User, on_delete=models.CASCADE)
     content = models.TextField()
-    create_date = models.DateTimeField() 
-    modify_date = models.DateTimeField(null=True, blank=True) #수정 일시
-    voter = models.ManyToManyField(User, related_name='voter_comment')
+    create_date = models.DateTimeField()
+    modify_date = models.DateTimeField(null=True, blank=True)
+    question = models.ForeignKey(Question, null=True, blank=True, on_delete=models.CASCADE)
+    answer = models.ForeignKey(Answer, null=True, blank=True, on_delete=models.CASCADE)
