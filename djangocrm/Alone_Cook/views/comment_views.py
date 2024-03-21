bf54fb1fffc42c6eecf36e06a89e1598e4288357
Gwonhee Jo <gwonheej@gmail.com>
2024-03-21 21:59:32 +0900

    comment ver2

--- djangocrm/Alone_Cook/views/comment_views.py
+++ djangocrm/Alone_Cook/views/comment_views.py
@@ -1,8 +1,14 @@
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+
+from ..forms import CommentForm
+from ..models import Answer, Comment
+
 @login_required(login_url='common:login')
 def comment_create_answer(request, answer_id):
-    """
-    pybo 답글댓글등록
-    """
+
     answer = get_object_or_404(Answer, pk=answer_id)
     if request.method == "POST":
         form = CommentForm(request.POST)
@@ -12,22 +18,20 @@ def comment_create_answer(request, answer_id):
             comment.create_date = timezone.now()
             comment.answer = answer
             comment.save()
-            return redirect('pybo:detail', question_id=comment.answer.question.id)
+            return redirect('Alone_Cook:detail', question_id=comment.answer.question.id)
     else:
         form = CommentForm()
     context = {'form': form}
-    return render(request, 'pybo/comment_form.html', context)
+    return render(request, 'Alone_Cook/comment_form.html', context)
 
 
 @login_required(login_url='common:login')
 def comment_modify_answer(request, comment_id):
-    """
-    pybo 답글댓글수정
-    """
+
     comment = get_object_or_404(Comment, pk=comment_id)
     if request.user != comment.author:
         messages.error(request, '댓글수정권한이 없습니다')
-        return redirect('pybo:detail', question_id=comment.answer.question.id)
+        return redirect('Alone_Cook:detail', question_id=comment.answer.question.id)
 
     if request.method == "POST":
         form = CommentForm(request.POST, instance=comment)
@@ -35,22 +39,20 @@ def comment_modify_answer(request, comment_id):
             comment = form.save(commit=False)
             comment.modify_date = timezone.now()
             comment.save()
-            return redirect('pybo:detail', question_id=comment.answer.question.id)
+            return redirect('Alone_Cook:detail', question_id=comment.answer.question.id)
     else:
         form = CommentForm(instance=comment)
     context = {'form': form}
-    return render(request, 'pybo/comment_form.html', context)
+    return render(request, 'Alone_Cook/comment_form.html', context)
 
 
 @login_required(login_url='common:login')
 def comment_delete_answer(request, comment_id):
-    """
-    pybo 답글댓글삭제
-    """
+
     comment = get_object_or_404(Comment, pk=comment_id)
     if request.user != comment.author:
         messages.error(request, '댓글삭제권한이 없습니다')
-        return redirect('pybo:detail', question_id=comment.answer.question.id)
+        return redirect('Alone_Cook:detail', question_id=comment.answer.question.id)
     else:
         comment.delete()
-    return redirect('pybo:detail', question_id=comment.answer.question.id)
+    return redirect('Alone_Cook:detail', question_id=comment.answer.question.id)
