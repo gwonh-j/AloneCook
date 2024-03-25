f297b49165ba1fdcc831ab7fcacdeb6a5362c3a6
Gwonhee Jo <gwonheej@gmail.com>
2024-03-25 23:19:52 +0900

    ingredient ver2

---
+++ djangocrm/Alone_Cook/views/ingredient_views.py
@@ -0,0 +1,7 @@
+from django.contrib import messages
+from django.contrib.auth.decorators import login_required
+from django.shortcuts import render, get_object_or_404, redirect
+from django.utils import timezone
+
+from ..forms import IngredientForm
+from ..models import Answer, Comment, ingredient_list
